1b7f904708e459125f15c518aad385f39fe33dd0
Victoria <>
2023-04-03 02:01:38 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Informe a primeira nota: ");
+    double n1 = double.Parse(Console.ReadLine());
+    
+    Console.WriteLine ("Informe a segunda nota: ");
+    double n2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a terceira nota: ");
+    double n3 = double.Parse(Console.ReadLine());
+
+    double media = (n1+n2+n3)/3;
+
+    Console.WriteLine("A média é " + media);
+    
+  }
+}
