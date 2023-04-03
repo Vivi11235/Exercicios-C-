0d66a614414e48d023feb27cc80c4677dafabe92
Victoria <114111252+Vivi11235@users.noreply.github.com>
2023-04-03 02:24:56 +0000

    Acréscimo de if-else no cálculo da média

--- main.cs
+++ main.cs
@@ -1,19 +1,29 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Informe a primeira nota: ");
-    double n1 = double.Parse(Console.ReadLine());
-    
-    Console.WriteLine ("Informe a segunda nota: ");
-    double n2 = double.Parse(Console.ReadLine());
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Informe a primeira nota: ");
+        double n1 = double.Parse(Console.ReadLine());
 
-    Console.WriteLine("Informe a terceira nota: ");
-    double n3 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Informe a segunda nota: ");
+        double n2 = double.Parse(Console.ReadLine());
 
-    double media = (n1+n2+n3)/3;
+        Console.WriteLine("Informe a terceira nota: ");
+        double n3 = double.Parse(Console.ReadLine());
 
-    Console.WriteLine("A média é " + media);
-    
-  }
+        double media = (n1 + n2 + n3) / 3;
+
+        Console.WriteLine("A média é " + media);
+
+        if(media>=7){
+          Console.WriteLine("Aprovado por média.");
+        }else if(media<3){
+          Console.WriteLine("Reprovado.");   
+        }else{
+          Console.WriteLine("De recuperação.");
+        }
+
+    }
 }
